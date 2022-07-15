75f57297f2067c3a8fec2a2e5ceef6e9010f5251
Anderson <andersonmendesleandro@gmail.com>
2022-07-15 14:57:30 -0300

    Atividade finalizada

--- products.js
+++ products.js
@@ -88,3 +88,74 @@ const products = [
     imageDescription: "",
   },
 ];
+
+let ulFrutas = document.createElement("ul")
+let ulBebidas = document.createElement("ul")
+let ulHigiene = document.createElement("ul")
+
+criarCard(products)
+function criarCard(listaProdutos) {
+
+  for (let i = 0; i < listaProdutos.length; i++) {
+
+    let li = document.createElement("li")
+    li.setAttribute("class", "product")
+
+
+    let img = document.createElement("img")
+    if (listaProdutos[i].image == undefined) {
+      img.setAttribute("src", "./img/products/no-img.svg")
+    } else {
+      img.setAttribute("src", `${listaProdutos[i].image}`)
+    }
+    img.setAttribute("alt", `Imagem de um(a) ${listaProdutos[i].title}`)
+    img.setAttribute("title", `Imagem de um(a) ${listaProdutos[i].title}`)
+    img.setAttribute("class", "product-img")
+
+
+    let main = document.createElement("main")
+    main.setAttribute("class", "product-main")
+
+
+    let h1 = document.createElement("h1")
+    h1.setAttribute("class", "product-title")
+    h1.innerText = listaProdutos[i].title
+
+
+    let h5 = document.createElement("h5")
+    h5.setAttribute("class", "product-category")
+    h5.innerText = listaProdutos[i].category
+
+
+    let strong = document.createElement("strong")
+    strong.setAttribute("class", "product-price")
+    strong.innerText = `R$ ${listaProdutos[i].price.toFixed(2)}`
+
+
+    main.append(h1, h5, strong)
+
+    li.append(img, main)
+
+    let categoria = listaProdutos[i].category
+
+    adicionaProdutoUl(li, categoria)
+  }
+}
+
+
+function adicionaProdutoUl(li, categoria) {
+
+  if (categoria == "Frutas") {
+    ulFrutas.appendChild(li)
+  }
+  else if (categoria == "Bebidas") {
+    ulBebidas.appendChild(li)
+  }
+  else if (categoria == "Higiene") {
+    ulHigiene.appendChild(li)
+  }
+}
+
+document.querySelector(".fruits").appendChild(ulFrutas)
+document.querySelector(".drinks").appendChild(ulBebidas)
+document.querySelector(".hygiene").appendChild(ulHigiene)
